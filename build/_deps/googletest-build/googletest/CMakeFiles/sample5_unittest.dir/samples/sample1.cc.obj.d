4ecaf72afb180fe538c82a173df0904060ba3722
potolok86 <potolok86@gmail.com>
2025-01-27 20:38:50 +0300

    корректировка yml-файла. убран переход в папку

---
+++ build/_deps/googletest-build/googletest/CMakeFiles/sample5_unittest.dir/samples/sample1.cc.obj.d
@@ -0,0 +1,3 @@
+_deps/googletest-build/googletest/CMakeFiles/sample5_unittest.dir/samples/sample1.cc.obj: \
+ C:\Projects\example7\build\_deps\googletest-src\googletest\samples\sample1.cc \
+ C:\Projects\example7\build\_deps\googletest-src\googletest\samples\sample1.h
---
+++ build/mytests[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "C:/Projects/example7/build/mytests[1]_tests.cmake")
+  include("C:/Projects/example7/build/mytests[1]_tests.cmake")
+else()
+  add_test(mytests_NOT_BUILT mytests_NOT_BUILT)
+endif()
